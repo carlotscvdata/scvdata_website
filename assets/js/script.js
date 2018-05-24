b6463b16d1308fd92e6416b2e90509db5323ee0d
Carlo Teran <carlot_github@scvdata.com>
2018-05-24 14:59:46 -0700

    Fix JS and CSS, fadeIn

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,4 +3,10 @@ $(document).ready(function(){
     $('.parallax').parallax();
     $(".button-collapse").sideNav();
   });
-      
+
+var options = [
+  {selector: '.who-we-help-fade', offset: 0, callback: 'Materialize.fadeInImage(".who-we-help-fade")' },
+  {selector: '.clients-fade', offset: 0, callback: 'Materialize.fadeInImage(".clients-fade")' }
+];
+
+Materialize.scrollFire(options);  
